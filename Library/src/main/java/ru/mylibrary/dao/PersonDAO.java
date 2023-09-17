0da0c78d4638181a3c5d3e3dde2083f4993bce2c
Nikita <melianas@student.21-school.ru>
2023-09-18 02:15:21 +0300

    PersonDAO started

--- Library/src/main/java/ru/mylibrary/dao/PersonDAO.java
+++ Library/src/main/java/ru/mylibrary/dao/PersonDAO.java
@@ -1,4 +1,12 @@
 package ru.mylibrary.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class PersonDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
